85eeb9c47044aaa7db1b23abb82d5d2b707a1534
tristanbagnulo <tristanbagnulo1@gmail.com>
2021-03-29 17:54:49 +1100

    Maps Push #1
    Just added map. More changes to come.

--- app/src/main/java/com/example/infs3605/MainActivity.java
+++ app/src/main/java/com/example/infs3605/MainActivity.java
@@ -4,7 +4,10 @@ package com.example.infs3605;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btMap = findViewById(R.id.btMap);
+        btMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Code executes here on main thread after user presses button
+                Intent intent = new Intent(v.getContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
 
