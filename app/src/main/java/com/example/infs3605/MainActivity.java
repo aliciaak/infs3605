9b93ae0b524941c3ff0267bac26126963ed07a30
frownak <47545845+frownak@users.noreply.github.com>
2021-04-03 12:28:04 +1100

    bottom navigation bar (not working)

--- app/src/main/java/com/example/infs3605/MainActivity.java
+++ app/src/main/java/com/example/infs3605/MainActivity.java
@@ -2,12 +2,19 @@
 
 package com.example.infs3605;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Button btMap = findViewById(R.id.btMap);
         btMap.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
 }
 
 
