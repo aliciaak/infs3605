8920ba863d62d7690867d722947be3748cc0a104
linjunpeng <z5214087@ad.unsw.edu.au>
2021-04-06 18:05:29 +0800

    information page

--- app/src/main/java/com/example/infs3605/InformationFragment.java
+++ app/src/main/java/com/example/infs3605/InformationFragment.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.example.infs3605.InformationPages.AboutEmblems;
 
@@ -27,6 +28,9 @@ public class InformationFragment extends Fragment  {
     Button aboutEmblems;
     Button protectEmblems;
     Button emblemUsage;
+    Button goBack;
+    TextView textView;
+    TextView title;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,11 +43,65 @@ public class InformationFragment extends Fragment  {
         aboutEmblems = v.findViewById(R.id.button2);
         protectEmblems = v.findViewById(R.id.button3);
         emblemUsage = v.findViewById(R.id.button4);
+
+        textView = v.findViewById(R.id.textView12);
+        title = v.findViewById(R.id.pageTitle);
+
+        goBack = v.findViewById(R.id.btn_back);
+
         aboutRedCross.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), AboutEmblems.class);
-                startActivity(intent);
+//                Intent intent = new Intent(getActivity(), AboutEmblems.class);
+//                startActivity(intent);
+                textView.setText("About RedCross");
+                textView.setVisibility(View.VISIBLE);
+                aboutRedCross.setVisibility(View.INVISIBLE);
+                aboutEmblems.setVisibility(View.INVISIBLE);
+                protectEmblems.setVisibility(View.INVISIBLE);
+                emblemUsage.setVisibility(View.INVISIBLE);
+                title.setVisibility(View.INVISIBLE);
+                goBack.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        aboutEmblems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("About emblem");
+                textView.setVisibility(View.VISIBLE);
+                aboutRedCross.setVisibility(View.INVISIBLE);
+                aboutEmblems.setVisibility(View.INVISIBLE);
+                protectEmblems.setVisibility(View.INVISIBLE);
+                emblemUsage.setVisibility(View.INVISIBLE);
+                title.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        protectEmblems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Protect emblem");
+                textView.setVisibility(View.VISIBLE);
+                aboutRedCross.setVisibility(View.INVISIBLE);
+                aboutEmblems.setVisibility(View.INVISIBLE);
+                protectEmblems.setVisibility(View.INVISIBLE);
+                emblemUsage.setVisibility(View.INVISIBLE);
+                title.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        emblemUsage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("emblemUsage");
+                textView.setVisibility(View.VISIBLE);
+                aboutRedCross.setVisibility(View.INVISIBLE);
+                aboutEmblems.setVisibility(View.INVISIBLE);
+                protectEmblems.setVisibility(View.INVISIBLE);
+                emblemUsage.setVisibility(View.INVISIBLE);
+                title.setVisibility(View.INVISIBLE);
             }
         });
 
